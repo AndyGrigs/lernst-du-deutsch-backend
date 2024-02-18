64e1a7208f3cd9b8da99620ac35b2c6fd4f4541e
AndyGrigs <97991761+AndyGrigs@users.noreply.github.com>
2024-02-18 08:56:40 +0100

    +

--- models/exercise.js
+++ models/exercise.js
@@ -1,17 +1,22 @@
 import mongoose from "mongoose";
 
-const taskSchema = new mongoose.Schema({
+const taskSchema = new mongoose.Schema(
+  {
     content: String,
     solution: [String],
-});
+  }
+  // {
+  //   _id: false,
+  // }
+);
 
 const exerciseSchema = new mongoose.Schema({
-    number: Number,
-    instruction: String,
-    example: String,
-    tasks: [taskSchema],
+  number: Number,
+  instruction: String,
+  example: String,
+  tasks: [taskSchema],
 });
 
-const Exercise = mongoose.model('Exercise', exerciseSchema);
+const Exercise = mongoose.model("Exercise", exerciseSchema);
 
-export default Exercise
+export default Exercise;
